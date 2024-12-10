0e99c2e8e92dbac8fe83ea1e7507467f67778a49
toel-kamanda34 <toelokemwa@gmail.com>
2024-12-11 01:23:44 +0300

    building functions

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,38 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+const backpack = {
+  name: "Everyday Backpack",
+  volume: 4,
+  size: "medium",
+};
+
+function newBackPack(bag) {
+  const newBag = document.createElement("article");
+  newBag.innerHTML = `
+        <h1>${bag.name}</h1>
+        <ul>
+        <li>Volume : ${bag.volume}</li>
+        <li>Size : ${bag.size}</li>
+        </ul>
+`;
+  return newBag;
+}
+const main = document.querySelector("main");
+main.append(newBackPack(backpack));
+
+const newBackPack1 = (bag) => {
+  const newBag = document.createElement("article");
+  newBag.innerHTML = `
+          <h1>${bag.name}</h1>
+          <ul>
+          <li>Volume : ${bag.volume}</li>
+          <li>Size : ${bag.size}</li>
+          </ul>
+  `;
+  return newBag;
+};
+
+const main1 = document.querySelector("main");
+main1.append(newBackPack1(backpack));
